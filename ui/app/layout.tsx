4009b990afb78d7dd597b4bad46634a6809d5597
mostafakhajezade <mkhajezade@gmail.com>
2025-05-29 17:51:33 +0330

    feat: add Tailwind CSS

---
+++ ui/app/layout.tsx
@@ -0,0 +1,34 @@
+// app/layout.tsx
+
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: 'My App',
+  description: '...',
+};
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
